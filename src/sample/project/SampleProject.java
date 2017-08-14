9e6f0670426cae845a552badd7c80fe158d1ab73
ilmd <30865309+ilmd@users.noreply.github.com>
2017-08-14 12:20:42 +0530

    Update SampleProject.java

--- src/sample/project/SampleProject.java
+++ src/sample/project/SampleProject.java
@@ -17,7 +17,7 @@ public class SampleProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("I  CHANGED HERE!!!");
+        System.out.println("I  CHANGED lakkkk!!!");
     }
     
 }
